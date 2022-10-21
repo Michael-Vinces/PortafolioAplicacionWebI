d555110a5686af32bf98e766612165a8602a928c
Michael Leonardo Vinces Andrade <92233211+Michael-Vinces@users.noreply.github.com>
2022-10-21 14:48:58 -0500

    Add files via upload

---
+++ Proyecto-InventarioUleam/JavaScript/Facultades.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menuToggle =document.querySelector('.toggle');
+    const sidebar =document.querySelector('.sidebar');               
+    sidebar.classList.toggle('active')
+    menuToggle.classList.toggle('active')
+
+}
